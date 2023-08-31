7a909366ba1bb69c24f58216fe5713ca10b1ea6c
sejal99 <sejal8974@gmail.com>
2023-09-01 00:29:08 +0530

    news commit

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -19,12 +19,7 @@ function About() {
         <div className='mt-8 md:ml-[150px] grid grid-cols-1 gap-4 sm:grid-cols-3 md:mt-16 lg:gap-x-12'></div>
         <div className='mt-8 text-center md:mt-16'>
           <a href='https://gyandeeparyan-dev.netlify.app/#projects' target="_blank">
-            <button
-              type='button'
-              className='rounded-md bg-black px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-black/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black'
-            >
-              MORE PROJECTS
-            </button>
+           
           </a>
         </div>
       </div>
